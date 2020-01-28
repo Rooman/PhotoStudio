b99af1b9c8021678fd5d5464492d05830b8e70e9
Yevhenii <eugene.baginsky@gmail.com>
2020-01-28 15:39:26 +0200

    Scripts for DB

---
+++ db/schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE UserRole (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    roleName VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(400) UNIQUE NOT NULL,
+    phoneNumber BIGINT UNSIGNED,
+    firstName VARCHAR(100),
+    lastName VARCHAR(100),
+    gender VARCHAR(10) CHECK(gender IN ('M', 'F')),
+    userRoleId INT NOT NULL,
+    passwordHash VARCHAR(200) NOT NULL,
+    salt VARCHAR(500) NOT NULL,
+    country VARCHAR(50),
+    city VARCHAR(50),
+    zip INT UNSIGNED,
+    street VARCHAR(100),
+    buildingNumber INT UNSIGNED,
+    INDEX(email, phoneNumber),
+    FOREIGN KEY (userRoleId) REFERENCES UserRole(id)
+);
+
+CREATE TABLE OrderStatus (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    statusName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Orders (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    oderDate TIMESTAMP NOT NULL,
+    statusId INT NOT NULL,
+    userId INT NOT NULL,
+    orderComment VARCHAR(500),
+    FOREIGN KEY (statusId) REFERENCES OrderStatus(id),
+    FOREIGN KEY (userId) REFERENCES Users(id)
+);
+
+CREATE TABLE PhotoStatus (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    statusName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE OrderPhotos (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    photoSource VARCHAR(200) NOT NULL,
+    orderId INT NOT NULL,
+    photoStatusId INT NOT NULL,
+    FOREIGN KEY (orderId) REFERENCES Orders(id),
+    FOREIGN KEY (photoStatusId) REFERENCES PhotoStatus(id)
+);
