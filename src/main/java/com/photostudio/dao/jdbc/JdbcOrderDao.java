bed3af59787ef11e65bdad0ab0fc3b65fa75750f
Yevhenii <eugene.baginsky@gmail.com>
2020-01-30 00:17:33 +0200

    Added function show all orders: 1. Created Servlet for work with DB and SELECT request to DB 2. Created entity Order 3. Created Service and Servlet for work with parameters 4. In html file created only table with orders, because we don't have design project

---
+++ src/main/java/com/photostudio/dao/jdbc/JdbcOrderDao.java
@@ -0,0 +1,49 @@
+package com.photostudio.dao.jdbc;
+
+import com.photostudio.dao.OrderDao;
+import com.photostudio.dao.jdbc.mapper.OrderRowMapper;
+import com.photostudio.entity.Order;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcOrderDao implements OrderDao {
+
+    private static final String GET_ALL_ORDERS = "SELECT o.id id," +
+            "os.statusName statusName, " +
+            "o.orderDate orderDate, " +
+            "u.email email, " +
+            "u.phoneNumber phoneNumber, " +
+            "o.comment comment " +
+            "FROM Orders o " +
+            "JOIN OrderStatus os ON o.statusId = os.id " +
+            "JOIN Users u ON o.userId = u.id";
+
+    private static final OrderRowMapper ORDER_ROW_MAPPER = new OrderRowMapper();
+    private DataSource dataSource;
+
+    public JdbcOrderDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public List<Order> getAll() {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_ORDERS);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            List<Order> orders = new ArrayList<>();
+            while (resultSet.next()) {
+                Order order = ORDER_ROW_MAPPER.mapRow(resultSet);
+                orders.add(order);
+            }
+            return orders;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error during get all products", e);
+        }
+    }
+}
