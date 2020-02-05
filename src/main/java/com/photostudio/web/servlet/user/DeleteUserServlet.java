19649bf0fbf7fa455c3dcf069003a875ad929f28
ValentynaBondarenko <52756670+ValentynaBondarenko@users.noreply.github.com>
2020-02-05 15:57:43 +0200

    Update DeleteUserServlet.java

--- src/main/java/com/photostudio/web/servlet/user/DeleteUserServlet.java
+++ src/main/java/com/photostudio/web/servlet/user/DeleteUserServlet.java
@@ -8,11 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DeleteUserServlet extends HttpServlet {
+private static final String ALL_USERS_PAGE = "/admin/users";
     private UserService userService = ServiceLocator.getService(UserService.class);
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
         long id = Long.parseLong(request.getParameter("id"));
         userService.delete(id);
+
+        response.sendRedirect(ALL_USERS_PAGE);
     }
 }
