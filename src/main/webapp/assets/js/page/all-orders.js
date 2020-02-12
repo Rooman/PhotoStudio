9370e499e7efa369612fc2309f1a589bfdd15028
ValentynaBondarenko <ValentynaBondarenkoo@gmail.com>
2020-02-12 23:02:01 +0200

    b-5-get-all-orders interim commit

--- src/main/webapp/assets/js/page/all-orders.js
+++ src/main/webapp/assets/js/page/all-orders.js
@@ -1,5 +1,5 @@
 function showHideFilterTable(element) {
-  var srcElement = document.getElementById(element);
+  let srcElement = document.getElementById(element);
 	 if (srcElement != null) {
 		if (srcElement.style.display == "block") {
 			srcElement.style.display = 'none';
