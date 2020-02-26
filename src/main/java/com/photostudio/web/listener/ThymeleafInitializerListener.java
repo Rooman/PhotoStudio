bbfdb3a7a8febc266fd1c94318f53190e83b6a50
ValentynaBondarenko <ValentynaBondarenkoo@gmail.com>
2020-02-27 01:26:02 +0200

    b-17-add-annotations 1.Delete web.xml; 2.Add @WebListener annotation

--- src/main/java/com/photostudio/web/listener/ThymeleafInitializerListener.java
+++ src/main/java/com/photostudio/web/listener/ThymeleafInitializerListener.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class ThymeleafInitializerListener implements ServletContextListener {
     private final Logger LOG = LoggerFactory.getLogger(getClass());
 
