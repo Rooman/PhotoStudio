d65f9b001f3e7db42505fd432c2d9007548c3ad4
OleksandrGubariev <gubarev.alexandr@gmail.com>
2020-02-28 19:22:25 +0200

    1.Fix bugs in EncodingFilter 2.add log in EncodingFilter

--- src/main/java/com/photostudio/web/filter/EncodingFilter.java
+++ src/main/java/com/photostudio/web/filter/EncodingFilter.java
@@ -1,5 +1,7 @@
 package com.photostudio.web.filter;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
@@ -7,22 +9,19 @@ import java.io.IOException;
 
 @WebFilter(urlPatterns = {"/*"},
         initParams = @WebInitParam(name = "requestEncoding", value = "UTF-8"))
+@Slf4j
 public class EncodingFilter implements Filter {
     private String encoding;
 
     public void init(FilterConfig config) {
         encoding = config.getInitParameter("requestEncoding");
-        if (encoding == null) {
-            encoding = "UTF-8";
-        }
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        log.debug("Encoding filter start with encoding:{}", encoding);
         request.setCharacterEncoding(encoding);
         chain.doFilter(request, response);
     }
 
-    public void destroy() {
-    }
 }
