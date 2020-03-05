fbd965fa5c2389ca27f62d148eb12fafe7ba0f21
nomar <nomarchia2@gmail.com>
2020-03-05 16:41:27 +0200

    b-21-add-multi-language-support
    Latest changes

---
+++ src/main/java/com/photostudio/web/filter/CookieLocaleFilter.java
@@ -0,0 +1,28 @@
+package com.photostudio.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "CookieLocaleFilter", urlPatterns = {"/*"})
+public class CookieLocaleFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        Cookie cookie;
+        if (req.getParameter("cookieLocale") != null) {
+            cookie = new Cookie("lang", req.getParameter("cookieLocale"));
+        } else {
+            cookie = new Cookie("lang", "en");
+        }
+        res.addCookie(cookie);
+
+        chain.doFilter(request, response);
+    }
+}
--- src/main/java/com/photostudio/web/util/CommonVariableAppendService.java
+++ src/main/java/com/photostudio/web/util/CommonVariableAppendService.java
@@ -4,12 +4,22 @@ import com.photostudio.security.entity.Session;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
+import java.util.Objects;
 
 public class CommonVariableAppendService {
     public static void appendUser(Map<String, Object> paramsMap, HttpServletRequest request) {
         Session session = (Session) request.getAttribute("session");
         if (session != null) {
             paramsMap.put("user", session.getUser());
+        }
+    }
+
+    public static void appendLang(Map<String, Object> paramsMap, HttpServletRequest request) {
+        String lang = (String) request.getAttribute("lang");
+        if (lang != null) {
+            paramsMap.put("language", lang);
+        } else {
+            paramsMap.put("language", "en");
         }
     }
 }
