9f53149a0b6d517e0ad88eee65ba03d61851ac89
Yuliia-Sh <uliya1301@rambler.ru>
2020-03-05 12:26:36 +0200

    added table languages, added column to table Users

--- src/main/resources/db/migration/V1_10__add_language.sql
+++ src/main/resources/db/migration/V1_10__add_language.sql
@@ -3,3 +3,12 @@ CREATE TABLE Languages (
     short_name VARCHAR(3) NOT NULL,
     full_name  VARCHAR(15)
 ) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO Languages (short_name, full_name) VALUES ('ENG', 'English');
+INSERT INTO Languages (short_name, full_name) VALUES ('GER', 'Deutsch');
+INSERT INTO Languages (short_name, full_name) VALUES ('RUS', 'Russian');
+COMMIT;
+
+ALTER TABLE Users
+ADD COLUMN langId INT DEFAULT(1),
+ADD FOREIGN KEY (langId) REFERENCES Languages(id);
